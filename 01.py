94ed42795bc43328e09a99b9e97b7bd2a9695433
his0822 <pnu40223@gmail.com>
2024-03-06 13:11:59 +0900

    20240306

--- 01.py
+++ 01.py
@@ -1,6 +1,40 @@
-N = int(input())
-x = "*"
-y = " "
-if (1 <= N <= 100):
-    for i in range(1,N+1):
-        print(y*(N-i)+x*i)
+def func1(humidity, val_set):
+    if humidity < val_set:
+        return 3
+
+    return 1
+
+def func2(humidity):
+    if humidity >= 50:
+        return 0
+    elif humidity >= 40:
+        return 1
+    elif humidity >= 30:
+        return 2
+    elif humidity >= 20:
+        return 3
+    elif humidity >= 10:
+        return 4
+    
+
+        
+
+
+def func3(humidity, val_set):
+    if humidity < val_set:
+        return 1
+    return 0
+
+
+def solution(mode_type, humidity, val_set):
+    answer = 0
+    if mode_type == "auto":
+        answer = func2
+
+    elif mode_type == "target":
+        answer = func1
+
+    elif mode_type == "minimum":
+        answer = func3
+
+    return answer
---
+++ baekjoon/8393.py
@@ -0,0 +1,6 @@
+n = int(input())
+if (1<= n <= 10000):
+    for i in range(1,n+1):
+        i = i+1
+        
+print(i)
